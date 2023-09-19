08ec88b9db20d2047da22ab09feb45ea172108a8
Dustin Blackman <dev@dustinblackman.com>
2023-09-19 10:37:10 -0400

    chore: Remove tracking

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -e
 
 TAG="v0.51.9"
 
-DOCKER_DEFAULT_PLATFORM=linux/amd64 docker build -t ghcr.io/dustinblackman/porter:"$TAG" -f ./docker/Dockerfile .
-docker push ghcr.io/dustinblackman/porter:"$TAG"
+DOCKER_DEFAULT_PLATFORM=linux/amd64 docker build -t dustinblackman/porter:"$TAG" -f ./docker/Dockerfile .
+docker push dustinblackman/porter:"$TAG"
